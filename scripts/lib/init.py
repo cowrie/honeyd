1ab18610516abc2b3346c277edde2f8a0428c19e
Michel Oosterhof <michel@oosterhof.net>
2025-12-03 13:32:08 +0800

    don't use bare except

--- scripts/lib/init.py
+++ scripts/lib/init.py
@@ -9,10 +9,10 @@ InitializeDB(home + "/.config/honeyd/names")
 
 try:
 	os.chmod(home + "/.config/honeyd/names", 0o666)
-except:
+except Exception:
 	pass
 
 try:
 	os.chmod(home + "/.config/honeyd", 0o777)
-except:
+except Exception:
 	pass
